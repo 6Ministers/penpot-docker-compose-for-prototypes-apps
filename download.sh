1beb0d0509bea7f91befa45817a2c2cbdae87bf3
6ministers <manyatovskiy@gmail.com>
2023-12-25 19:35:53 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 DESTINATION=$1
 
 # clone Flectra directory
-git clone --depth=1 https://github.com/6Ministers/penpot-for-business $DESTINATION
+git clone --depth=1 https://github.com/6Ministers/penpot-for-business-apps $DESTINATION
 rm -rf $DESTINATION/.git
 
 
